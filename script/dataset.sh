9ce4bd53b7a6fcc3d993c821c5be63a7694cca9a
Itachi6912110 <itachi691211011129@gmail.com>
2019-04-30 04:41:35 +0800

    new data zip pos

--- script/dataset.sh
+++ script/dataset.sh
@@ -13,8 +13,8 @@ mkdir -p loss
 
 
 # get training data (preprocessed)
-wget --no-check-certificate "https://drive.google.com/uc?export=download&id=1FqWzk6qWEp80RWbFV4MCAhmSXX07uUEy" -O data/TONAS_note.zip
-wget --no-check-certificate "https://drive.google.com/uc?export=download&id=1Fo1yL3S0KnJ3KcOcK7x5T-KTmjDc0jO1" -O data/ISMIR2014_note.zip
+wget --no-check-certificate "https://drive.google.com/uc?export=download&id=1HkOSutpUS_6Gx5Zic3SeQBbcSpBl-iKD" -O data/TONAS_note.zip
+wget --no-check-certificate "https://drive.google.com/uc?export=download&id=12caWqjm_Wnaa50HOfR1wOYUtr7E4IW5M" -O data/ISMIR2014_note.zip
 wget --no-check-certificate "https://drive.google.com/uc?export=download&id=132b_iGgMtVA9bNmsPGJDo8_k9EfAnkpJ" -O ans/TONAS_SDT6.zip
 wget --no-check-certificate "https://drive.google.com/uc?export=download&id=1ITHDTxLYLie02z-sD_823phvNcg8-Lc1" -O ans/TONAS_ans.zip
 wget --no-check-certificate "https://drive.google.com/uc?export=download&id=1_fxgSF1-g5c9G_lG7Cq_3ZQ_sueZTf02" -O ans/ISMIR2014_ans.zip
